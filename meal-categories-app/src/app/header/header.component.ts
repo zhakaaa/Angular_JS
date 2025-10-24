c0168c04130b180688273ae21ca4eee13e3e0908
zhakaaa <zhaka9072@gmail.com>
2025-10-24 23:27:29 +0500

    Initial Midterm Project

---
+++ meal-categories-app/src/app/header/header.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  imports: [
+    RouterLink
+  ],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+}
